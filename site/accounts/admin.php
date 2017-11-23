da952e26dc5017d11635998232ba9a311e867318
MilieuOffensif <magicfinger@live.fr>
2017-11-23 09:04:58 +0100

    ajout de recherches graphique

---
+++ site/accounts/admin.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: admin
+password: >
+  $2a$10$vXU7brTzncuAhGl4TGW37eBAJbXEaF5sHguxk06rEEIfKBFIDZX8u
+email: elie.partouche@hear.fr
+language: fr
+role: admin
