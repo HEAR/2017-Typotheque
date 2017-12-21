f0a152a50d8d2b99c65ed5e54d5e9aabe75c92e1
Loïc Horellou <hello@loichorellou.net>
2017-12-21 09:43:23 +0100

    remove admin.php

--- site/accounts/admin.php
+++
@@ -1,8 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: admin
-password: >
-  $2a$10$tstRn9.51ntGzmhVYaiqSu348UDJ07nGrh5S.Ho6nE589T8rSq6SK
-email: loic.horellou@hear.fr
-language: fr
-role: admin
